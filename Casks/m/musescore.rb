c54ecb40f6ed7b9fb53482ab8314dc06d2de480c
PrudviTeja99 <prudvitejabandam987@gmail.com>
2026-09-05 00:11:44 +0530

    musescore: add linux support

--- Casks/m/musescore.rb
+++ Casks/m/musescore.rb
@@ -1,8 +1,38 @@
 cask "musescore" do
-  version "4.7.4.260706075,4.7.4"
-  sha256 "e3596e27da0806a3384cab67d52f8478ad21ed2bd6fc96d7cb874d840b016fac"
+  arch arm: "aarch64", intel: "x86_64"
+  url_end = on_system_conditional macos: ".dmg", linux: "-#{arch}.AppImage"
 
-  url "https://github.com/musescore/MuseScore/releases/download/v#{version.csv.second}/MuseScore-Studio-#{version.csv.first}.dmg"
+  version "4.7.4.260706075,4.7.4"
+
+  on_macos do
+    sha256 "e3596e27da0806a3384cab67d52f8478ad21ed2bd6fc96d7cb874d840b016fac"
+
+    app "MuseScore #{version.major}.app"
+    command_wrapper "mscore",
+                    executable: "#{appdir}/MuseScore #{version.major}.app/Contents/MacOS/mscore"
+
+    zap trash: [
+      "~/Library/Application Support/MuseScore",
+      "~/Library/Caches/MuseScore",
+      "~/Library/Caches/org.musescore.MuseScore",
+      "~/Library/Preferences/org.musescore.MuseScore*.plist",
+      "~/Library/Saved Application State/org.musescore.MuseScore.savedState",
+    ]
+  end
+  on_linux do
+    sha256 arm64_linux:  "162ae55b317660f196b2e73d566bdb45c1e990ed4cc140709d25d97b9ef366b0",
+           x86_64_linux: "9233ed1b87d3e6b45722278f3c286dcd41e83da778bd0f80a1dd04949696ad93"
+
+    app_image "MuseScore-Studio-#{version.csv.first}-#{arch}.AppImage", target: "MuseScore.AppImage"
+
+    zap trash: [
+      "~/.config/MuseScore",
+      "~/.local/share/data/MuseScore",
+      "~/.local/share/MuseScore",
+    ]
+  end
+
+  url "https://github.com/musescore/MuseScore/releases/download/v#{version.csv.second}/MuseScore-Studio-#{version.csv.first}#{url_end}"
   name "MuseScore"
   desc "Open-source music notation software"
   homepage "https://musescore.org/"
@@ -21,17 +51,4 @@ cask "musescore" do
   end
 
   auto_updates true
-  depends_on :macos
-
-  app "MuseScore #{version.major}.app"
-  command_wrapper "mscore",
-                  executable: "#{appdir}/MuseScore #{version.major}.app/Contents/MacOS/mscore"
-
-  zap trash: [
-    "~/Library/Application Support/MuseScore",
-    "~/Library/Caches/MuseScore",
-    "~/Library/Caches/org.musescore.MuseScore",
-    "~/Library/Preferences/org.musescore.MuseScore*.plist",
-    "~/Library/Saved Application State/org.musescore.MuseScore.savedState",
-  ]
 end
